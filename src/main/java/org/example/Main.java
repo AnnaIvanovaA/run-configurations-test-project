98780bed031d98d9b71e6eca9f046eec2554c55a
Anna Ivanova <anna.ivanova@jetbrains.com>
2026-04-08 18:00:18 +0200

    added infinite run loop with input

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,12 @@
 package org.example;
 
+import java.io.IOException;
+
 public class Main {
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) throws InterruptedException, IOException {
         System.out.println("Hello world!");
-        Thread.sleep(100000);
+        Thread.sleep(1000);
+        System.in.read();
         System.out.println();
     }
 }
